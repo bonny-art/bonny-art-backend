ac0741ae6da5a5888fb52ca75069bc4f83036859
bonny-art <svitlana.otenko@gmail.com>
2024-10-02 01:04:41 +0300

    Add husky

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-var': 'error',
+    'prefer-const': 'error',
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+  },
+};
