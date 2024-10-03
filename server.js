53fd613efb27ccdfccdee8c590097547ae576a5f
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-10-03 14:57:52 +0300

    added startup settings and connection to the database

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('dddd');
+import mongoose from 'mongoose';
+import app from './app.js';
+
+const { DB_HOST, PORT } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('Database connection successful');
+      console.log(`Server runing on ${PORT} PORT`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
