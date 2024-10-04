10ac4aa169d49c0cfaab30810e04c1507d0a3020
bonny-art <svitlana.otenko@gmail.com>
2024-10-04 15:59:58 +0300

    Add Master, Pattern and StitchPhoto schemas and models

---
+++ db/models/Master.ts
@@ -0,0 +1,9 @@
+import { Schema, model } from 'mongoose';
+
+const masterSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  purchasedPatterns: [{ type: Schema.Types.ObjectId, ref: 'Pattern' }],
+});
+
+export const Master = model('Master', masterSchema);
---
+++ db/models/StitchPhoto.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose';
+
+const stitchPhotoSchema = new Schema({
+  pattern: {
+    type: Schema.Types.ObjectId,
+    ref: 'Pattern',
+    required: true,
+  },
+  master: {
+    type: Schema.Types.ObjectId,
+    ref: 'Master',
+    required: true,
+  },
+  progress: { type: Number, required: true },
+  dateReceived: { type: Date, required: true },
+  imageUrl: { type: String, required: true },
+  episodeNumber: { type: Number, required: true },
+  numberWithinEpisode: { type: Number, required: true },
+});
+
+export const StitchPhoto = model('StitchPhoto', stitchPhotoSchema);
