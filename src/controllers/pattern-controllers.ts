83d696f90ada7799667aa793ec637a814b5e8ee8
bonny-art <svitlana.otenko@gmail.com>
2024-10-05 16:14:47 +0300

    Refactor controller to return structured data for all patterns

--- src/controllers/pattern-controllers.ts
+++ src/controllers/pattern-controllers.ts
@@ -1,15 +1,17 @@
-import { Response, Request, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
 import * as patternServices from '../services/pattern-services.js';
+import { GetAllPatternsRequest } from '@/types/patterns-type.js';
 
 export const getAllPatterns = async (
-  req: Request,
+  req: GetAllPatternsRequest,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
   try {
-    const allPatterns = await patternServices.getAllPatterns();
+    const language = req.query.language || 'uk';
+    const allPatterns = await patternServices.getAllPatterns(language);
 
-    res.send({ allPatterns });
+    res.send(allPatterns);
   } catch (error) {
     next(error);
   }
