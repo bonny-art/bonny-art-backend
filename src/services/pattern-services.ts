beb7ea3e5dcb5d5e89b3b25de171a8838a2094a5
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-10-05 23:40:46 +0300

    added helper extractWidthHeight

--- src/services/pattern-services.ts
+++ src/services/pattern-services.ts
@@ -5,15 +5,7 @@ import {
   PatternDb,
 } from '@/types/patterns-type.js';
 import { Pattern } from '../db/models/Pattern.js';
-
-const extractWidthHeight = (
-  codename: string
-): { width: number | null; height: number | null } => {
-  const widthHeightMatch = codename.match(/\((\d+)x(\d+)\)/);
-  const width = widthHeightMatch ? parseInt(widthHeightMatch[1]) : null;
-  const height = widthHeightMatch ? parseInt(widthHeightMatch[2]) : null;
-  return { width, height };
-};
+import { extractWidthHeight } from '../helpers/widthHeightExtractor.js';
 
 export const getAllPatterns = async (
   language: Language
