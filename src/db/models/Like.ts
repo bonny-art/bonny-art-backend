848d76bb38cbc00fbd6f224de48e8bacf04f5ea1
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-11-13 15:06:27 +0200

    added schema likes for pattern

---
+++ src/db/models/Like.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const likeSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  patternId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Pattern',
+    required: true,
+  },
+},
+{
+  versionKey: false, 
+});
+
+export default mongoose.model('Like', likeSchema);
