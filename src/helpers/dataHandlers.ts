5dcea5959e7ddd681e0bd7f3e51722513c959252
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-11-24 00:54:14 +0200

    change-of-imports-in-user-controllers

--- src/helpers/dataHandlers.ts
+++
@@ -1,9 +0,0 @@
-// export const getPatternDataByLanguage = (pattern: any, language: string) => {
-//   if (language === 'ru') {
-//     return pattern.title_ru;
-//   } else if (language === 'en') {
-//     return pattern.title_en;
-//   } else {
-//     return pattern.title;
-//   }
-// };
