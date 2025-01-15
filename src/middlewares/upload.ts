8982ddd4004380cb39a47c288054b0327d15f6de
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2025-01-15 16:40:26 +0200

    change-avatar-upload-parameters

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -3,6 +3,14 @@ import path from 'path';
 
 const destination = path.resolve('temp');
 
+// const allowedMimeTypes = [
+//   'image/jpeg',
+//   'image/png',
+//   'image/gif',
+//   'image/webp',
+//   'image/avif',
+// ];
+
 const multerConfig = multer.diskStorage({
   destination,
   filename: (req, file, cb) => {
@@ -12,6 +20,17 @@ const multerConfig = multer.diskStorage({
   },
 });
 
+// const fileFilter: multer.Options['fileFilter'] = (req, file, cb) => {
+//   if (!allowedMimeTypes.includes(file.mimetype)) {
+//     const error = new Error('File type not allowed') as
+//       | multer.MulterError
+//       | any;
+//     error.code = 'LIMIT_UNEXPECTED_FILE';
+//     return cb(error, false);
+//   }
+//   cb(null, true);
+// };
+
 const limits = {
   fileSize: 5 * 1024 * 1024,
 };
@@ -19,6 +38,7 @@ const limits = {
 const upload = multer({
   storage: multerConfig,
   limits,
+  // fileFilter,
 });
 
 export default upload;
