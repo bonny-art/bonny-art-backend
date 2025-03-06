4f0cf4eb083c7cc7f53566daa2d998c5d35b2728
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2025-03-07 00:06:54 +0200

    add-get-authors-router

--- src/controllers/filter-controllers.ts
+++ src/controllers/filter-controllers.ts
@@ -1,7 +1,7 @@
 import { Response, NextFunction } from 'express';
 import { setLanguageRequest } from '../types/common-types.js';
 import HttpError from '../helpers/http-error.js';
-import { getTitlesByLanguage } from '../services/filter-services.js';
+import { getAuthorsByLanguage, getTitlesByLanguage } from '../services/filter-services.js';
 
 export const getTitles = async (
   req: setLanguageRequest,
@@ -16,6 +16,25 @@ export const getTitles = async (
     const titles = await getTitlesByLanguage(lang);
 
     res.json({ titles });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAuthors = async (
+  req: setLanguageRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const lang = req.lang;
+    if (!lang) {
+      throw HttpError(404, 'Language was not set');
+    }
+
+    const authors = await getAuthorsByLanguage(lang);
+
+    res.json({ authors });
   } catch (error) {
     next(error);
   }
--- src/routes/api/filter-routes.ts
+++ src/routes/api/filter-routes.ts
@@ -5,5 +5,6 @@ import * as filterControllers from '../../controllers/filter-controllers.js';
 const filtersRouter = express.Router({ mergeParams: true });
 
 filtersRouter.get('/get-titles', setLanguage, filterControllers.getTitles);
+filtersRouter.get('/get-authors', setLanguage, filterControllers.getAuthors); 
 
 export default filtersRouter;
